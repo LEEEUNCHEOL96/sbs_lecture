adf0fe63c18502bc675049323aa109e8e50a5361
LEEEUNCHOEL <dmscjf1020@gmail.com>
2024-09-03 13:33:44 +0900

    init

---
+++ src/test/java/com/example/sbb1/Sbb1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sbb1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sbb1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
