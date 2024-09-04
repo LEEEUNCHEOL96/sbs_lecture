936655fa9e10f18f35e0f657ac8dcc9d87d6ab69
LEEEUNCHOEL <dmscjf1020@gmail.com>
2024-09-04 13:29:42 +0900

    Transactional_02-05-11

--- src/test/java/com/example/sbb1/Sbb1ApplicationTests.java
+++ src/test/java/com/example/sbb1/Sbb1ApplicationTests.java
@@ -3,6 +3,7 @@ package com.example.sbb1;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -112,4 +113,17 @@ class Sbb1ApplicationTests {
 		assertEquals(2, a.getQuestion().getId());
 	}
 
+	@Transactional
+	@Test  // 답변 데이터를 통해 질문 데이터 찾기 vs
+		// 질문 데이터를 통해 답변 데이터 찾기
+	void testJpa11() {
+		Optional<Question> oq = this.questionRepository.findById(2);
+		assertTrue(oq.isPresent());
+		Question q = oq.get();
+
+		List<Answer> answerList = q.getAnswerList();
+
+		assertEquals(1, answerList.size());
+		assertEquals("네 자동으로 생성됩니다.", answerList.get(0).getContent());
+	}
 }
