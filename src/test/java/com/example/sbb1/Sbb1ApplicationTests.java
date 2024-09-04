03976efb0480eb5c2738f44d0efff6c4eb54e8a7
LEEEUNCHOEL <dmscjf1020@gmail.com>
2024-09-04 13:28:11 +0900

    답변데이터조회_02-05-10

--- src/test/java/com/example/sbb1/Sbb1ApplicationTests.java
+++ src/test/java/com/example/sbb1/Sbb1ApplicationTests.java
@@ -104,4 +104,12 @@ class Sbb1ApplicationTests {
 		this.answerRepository.save(a);
 	}
 
+	@Test // 답변 데이터 조회하기
+	void testJpa10() {
+		Optional<Answer> oa = this.answerRepository.findById(1);
+		assertTrue(oa.isPresent());
+		Answer a = oa.get();
+		assertEquals(2, a.getQuestion().getId());
+	}
+
 }
