ee9f590acb32a5dbd3086785caace05147f80dd6
LEEEUNCHOEL <dmscjf1020@gmail.com>
2024-09-04 09:31:22 +0900

    출력_02-02

---
+++ src/main/java/com/example/sbb1/HelloController.java
@@ -0,0 +1,15 @@
+package com.example.sbb1;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+    @GetMapping("/hello")
+    @ResponseBody
+    public String helloWorld (){
+        return "Hello World !";
+    }
+
+}
---
+++ src/main/java/com/example/sbb1/MainController.java
@@ -0,0 +1,16 @@
+package com.example.sbb1;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/sbb")
+    @ResponseBody
+    public String index (){
+        return "안녕하세요 sbb 에 오신 것을 환영합니다.";
+    }
+}
+
