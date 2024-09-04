66570a3d7f8bdfbe635ada4c18ac180112aa9acc
LEEEUNCHOEL <dmscjf1020@gmail.com>
2024-09-04 13:27:35 +0900

    답변데이터저장_02-05-9

--- src/test/java/com/example/sbb1/Sbb1ApplicationTests.java
+++ src/test/java/com/example/sbb1/Sbb1ApplicationTests.java
@@ -90,4 +90,18 @@ class Sbb1ApplicationTests {
 		this.questionRepository.delete(q);
 		assertEquals(1, this.questionRepository.count());
 	}
+
+	@Test // 답변 데이터 저장하기
+	void testJpa9() {
+		Optional<Question> oq = this.questionRepository.findById(2);
+		assertTrue(oq.isPresent());
+		Question q = oq.get();
+
+		Answer a = new Answer();
+		a.setContent("네 자동으로 생성됩니다.");
+		a.setQuestion(q);  // 어떤 질문의 답변인지 알기위해서 Question 객체가 필요하다.
+		a.setCreateDate(LocalDateTime.now());
+		this.answerRepository.save(a);
+	}
+
 }
