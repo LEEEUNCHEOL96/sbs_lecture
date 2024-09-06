0979d4de8db5cf5ca00edb069235c4fed4626f70
LEEEUNCHOEL <dmscjf1020@gmail.com>
2024-09-06 10:30:55 +0900

    루트 URL 사용하기

--- src/main/java/com/example/sbb1/MainController.java
+++ src/main/java/com/example/sbb1/MainController.java
@@ -12,5 +12,10 @@ public class MainController {
     public String index (){
         return "안녕하세요 sbb 에 오신 것을 환영합니다.";
     }
+
+    @GetMapping("/")
+    public String root (){
+        return "redirect:/question/list";
+    }
 }
 
