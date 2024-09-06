99d48e9dc7ff757a836ae2b7722594900561b4c2
LEEEUNCHOEL <dmscjf1020@gmail.com>
2024-09-06 11:36:19 +0900

    URL 프리픽스 02-11

--- src/main/java/com/example/sbb1/question/QuestionController.java
+++ src/main/java/com/example/sbb1/question/QuestionController.java
@@ -5,15 +5,17 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
+@RequestMapping("/question")
 @RequiredArgsConstructor
 @Controller
 public class QuestionController {
     private final QuestionService questionService;
 
-    @GetMapping("/question/list")
+    @GetMapping("/list")
     public String list(Model model) {
         List<Question> questionList = this.questionService.getList();
         model.addAttribute("questionList", questionList);
@@ -21,7 +23,7 @@ public class QuestionController {
         return "question_list";
     }
 
-    @GetMapping("/question/detail/{id}")
+    @GetMapping("detail/{id}")
     public String getQuestion (Model model, @PathVariable("id") Integer id){
         Question question = this.questionService.getQuestion(id);
         model.addAttribute("question",question);
