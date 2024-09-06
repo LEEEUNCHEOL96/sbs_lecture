57463d5b0ca2cb7fddff5eb6cb18bb1a7a23ccf9
LEEEUNCHOEL <dmscjf1020@gmail.com>
2024-09-06 10:40:45 +0900

    서비스활용하기 02-09

---
+++ src/main/java/com/example/sbb1/question/QuestionService.java
@@ -0,0 +1,17 @@
+package com.example.sbb1.question;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Service
+public class QuestionService {
+    private final QuestionRepository questionRepository;
+
+    public List<Question> getList(){
+        return this.questionRepository.findAll();
+    }
+}
