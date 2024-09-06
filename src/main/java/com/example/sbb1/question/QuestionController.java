04a619de3283e2aa1a1f0664187814cd102fdeac
LEEEUNCHOEL <dmscjf1020@gmail.com>
2024-09-06 12:17:59 +0900

    답변만들기 02-11-1

--- src/main/java/com/example/sbb1/question/QuestionController.java
+++ src/main/java/com/example/sbb1/question/QuestionController.java
@@ -3,9 +3,7 @@ package com.example.sbb1.question;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -30,4 +28,9 @@ public class QuestionController {
         return "question_detail";
     }
 
+    @PostMapping("/create")
+    @ResponseBody
+    public String createQuestion () {
+        return "전송완료";
+    }
 }
