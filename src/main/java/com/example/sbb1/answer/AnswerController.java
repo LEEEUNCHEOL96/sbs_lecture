efe52f1e9667108b46d4005e967d8016fd763798
LEEEUNCHOEL <dmscjf1020@gmail.com>
2024-09-09 11:37:58 +0900

    02-16-6 answer_form

--- src/main/java/com/example/sbb1/answer/AnswerController.java
+++ src/main/java/com/example/sbb1/answer/AnswerController.java
@@ -2,13 +2,14 @@ package com.example.sbb1.answer;
 
 import com.example.sbb1.question.Question;
 import com.example.sbb1.question.QuestionService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @RequiredArgsConstructor
 @RequestMapping("/answer")
@@ -19,10 +20,16 @@ public class AnswerController {
 
     @PostMapping("/create/{id}")
     public String createAnswer (Model model, @PathVariable("id") Integer id,
-                                @RequestParam("content") String content){
+                                @Valid AnswerForm answerForm, BindingResult bindingResult){
+
         Question question = this.questionService.getQuestion(id);
 
-        this.answerService.create(question, content);
+        if(bindingResult.hasErrors()){
+            model.addAttribute("question",question);
+            return "question_detail";
+        }
+
+        this.answerService.create(question,answerForm.getContent());
         return String.format("redirect:/question/detail/%s", id);
     }
 
--- src/main/java/com/example/sbb1/question/QuestionController.java
+++ src/main/java/com/example/sbb1/question/QuestionController.java
@@ -1,5 +1,6 @@
 package com.example.sbb1.question;
 
+import com.example.sbb1.answer.AnswerForm;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
@@ -24,7 +25,7 @@ public class QuestionController {
     }
 
     @GetMapping("detail/{id}")
-    public String getQuestion (Model model, @PathVariable("id") Integer id){
+    public String getQuestion (Model model, @PathVariable("id") Integer id, AnswerForm answerForm){
         Question question = this.questionService.getQuestion(id);
         model.addAttribute("question",question);
         return "question_detail";
