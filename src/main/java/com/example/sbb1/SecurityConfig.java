de53cc385e374d7b31f4723934a2d895cfc144f9
LEEEUNCHOEL <dmscjf1020@gmail.com>
2024-09-12 11:21:22 +0900

    03-05-1 시큐리티 설치 및 설정

---
+++ src/main/java/com/example/sbb1/SecurityConfig.java
@@ -0,0 +1,22 @@
+package com.example.sbb1;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((authorizeHttpRequests) -> authorizeHttpRequests
+                        .requestMatchers(new AntPathRequestMatcher("/**")).permitAll())
+        ;  // 모든 페이지의 요청을 허락한다=로그인하지 않더라도 페이지 접근 가능
+        return http.build();
+    }
+}
