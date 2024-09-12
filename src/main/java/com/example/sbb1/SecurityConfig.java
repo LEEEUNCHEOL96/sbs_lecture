ba7d0dc4375a7569e72d1ff30da80b33e73651a9
LEEEUNCHOEL <dmscjf1020@gmail.com>
2024-09-12 12:26:10 +0900

    03-05-1 시큐리티 설ì  h2콘솔 설정

--- src/main/java/com/example/sbb1/SecurityConfig.java
+++ src/main/java/com/example/sbb1/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.header.writers.frameoptions.XFrameOptionsHeaderWriter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
@@ -16,7 +17,14 @@ public class SecurityConfig {
         http
                 .authorizeHttpRequests((authorizeHttpRequests) -> authorizeHttpRequests
                         .requestMatchers(new AntPathRequestMatcher("/**")).permitAll())
-        ;  // 모든 페이지의 요청을 허락한다=로그인하지 않더라도 페이지 접근 가능
+                .csrf((csrf) -> csrf
+                        // 모든 페이지의 요청을 허락한다=로그인하지 않더라도 페이지 접근 가능
+                        .ignoringRequestMatchers(new AntPathRequestMatcher("/h2-console/**")))
+                .headers((headers) -> headers
+                        .addHeaderWriter(new XFrameOptionsHeaderWriter(
+                                XFrameOptionsHeaderWriter.XFrameOptionsMode.SAMEORIGIN)))
+        ; //h2 콘솔 사용시 해킹방지 시큐리티기본 설정 해지 하는 라인
+
         return http.build();
     }
 }
