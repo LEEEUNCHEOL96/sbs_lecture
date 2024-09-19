3edefb72fa4488ffa10cd1c96dd56451db772320
LEEEUNCHOEL <dmscjf1020@gmail.com>
2024-09-19 12:49:07 +0900

    03-09-4 질문삭제기능 완료

--- src/main/java/com/example/sbb1/question/QuestionController.java
+++ src/main/java/com/example/sbb1/question/QuestionController.java
@@ -91,4 +91,17 @@ public class QuestionController {
 
         return String.format("redirect:/question/detail/%s", id);
     }
+
+    @PreAuthorize("isAuthenticated()")
+    @GetMapping("/delete/{id}")
+    public String questionDelete(Principal principal, @PathVariable("id") Integer id) {
+        Question question = this.questionService.getQuestion(id);
+
+        if (!question.getAuthor().getUsername().equals(principal.getName())) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "삭제권한이 없습니다.");
+        }
+        this.questionService.delete(question);
+
+        return "redirect:/";
+    }
 }
