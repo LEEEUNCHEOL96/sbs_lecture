fd6de0cc5b04b8f132e79f3ea1495d74d44597de
LEEEUNCHOEL <dmscjf1020@gmail.com>
2024-09-20 09:27:06 +0900

    03-09-5 답변수정 기능 완료

--- src/main/java/com/example/sbb1/answer/AnswerController.java
+++ src/main/java/com/example/sbb1/answer/AnswerController.java
@@ -6,13 +6,16 @@ import com.example.sbb1.user.SiteUser;
 import com.example.sbb1.user.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.security.Principal;
 
@@ -41,4 +44,16 @@ public class AnswerController {
         return String.format("redirect:/question/detail/%s", id);
     }
 
+    @PreAuthorize("isAuthenticated()")
+    @GetMapping("/modify/{id}")
+    public String answerModify(AnswerForm answerForm, @PathVariable("id")Integer id,Principal principal){
+        Answer answer = this.answerService.getAnswer(id);
+
+        if (!answer.getAuthor().getUsername().equals(principal.getName())) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "수정권한이 없습니다.");
+        }
+        answerForm.setContent(answer.getContent());
+
+        return "answer_form";
+    }
 }
--- src/main/java/com/example/sbb1/answer/AnswerService.java
+++ src/main/java/com/example/sbb1/answer/AnswerService.java
@@ -1,11 +1,13 @@
 package com.example.sbb1.answer;
 
+import com.example.sbb1.DataNotFoundException;
 import com.example.sbb1.question.Question;
 import com.example.sbb1.user.SiteUser;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
@@ -13,13 +15,30 @@ public class AnswerService {
 
     private final AnswerRepository answerRepository;
 
-
+    // 답변 기능
     public void create(Question question, String content, SiteUser author) {
         Answer answer = new Answer();
         answer.setContent(content);
         answer.setCreateDate(LocalDateTime.now());
         answer.setQuestion(question);
         answer.setAuthor(author);
+        this.answerRepository.save(answer);
+    }
+    // 답변수정 찾기 기능
+    public Answer getAnswer(Integer id){
+        Optional<Answer> optionalAnswer = this.answerRepository.findById(id);
+        if(optionalAnswer.isPresent()){
+            return optionalAnswer.get();
+        }
+        else {
+            throw new DataNotFoundException("answer not found");
+        }
+    }
+
+    // 답변수정 저장 기능
+    public void modify(Answer answer, String contenet){
+        answer.setContent(contenet);
+        answer.setModifyDate(LocalDateTime.now());
         this.answerRepository.save(answer);
     }
 }
