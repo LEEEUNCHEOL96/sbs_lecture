a7cc410333318297a30e883fd02225fd1cc85467
LEEEUNCHOEL <dmscjf1020@gmail.com>
2024-09-26 11:42:48 +0900

    ajax를 통한 답변 추천방법

--- src/main/java/com/example/sbb1/answer/AnswerController.java
+++ src/main/java/com/example/sbb1/answer/AnswerController.java
@@ -11,10 +11,7 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.security.Principal;
@@ -91,7 +88,7 @@ public class AnswerController {
         return String.format("redirect:/question/detail/%s", answer.getQuestion().getId());
     }
 
-    // 답변 좋아요 컨트롤러
+/*    // 답변 좋아요 컨트롤러
     @PreAuthorize("isAuthenticated()")
     @GetMapping("/vote/{id}")
     public String answerVote(@PathVariable("id") Integer id, Principal principal) {
@@ -102,5 +99,21 @@ public class AnswerController {
 
         return String.format("redirect:/question/detail/%s#answer_%s",
                 answer.getQuestion().getId(), answer.getId());
+    }*/
+
+
+    @PreAuthorize("isAuthenticated()")
+    @GetMapping("/vote/{id}")
+    @ResponseBody
+    public String answerVote(@PathVariable("id") Integer id, Principal principal) {
+        Answer answer = this.answerService.getAnswer(id);
+        SiteUser siteUser = this.userService.getUser(principal.getName());
+        this.answerService.vote(answer, siteUser);
+
+        Answer votedAnswer= this.answerService.getAnswer(id);
+        Integer count = votedAnswer.getVoter().size();
+
+        return  count.toString();
     }
+
 }
